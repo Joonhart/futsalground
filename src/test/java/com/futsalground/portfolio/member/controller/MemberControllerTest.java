4a483962365c8ea73abc3bf2ee95e2c816735762
장준하 <joonhart@gmail.com>
2020-11-29 17:04:31 +0900

    yml 파일 추가

---
+++ src/test/java/com/futsalground/portfolio/member/controller/MemberControllerTest.java
@@ -0,0 +1,17 @@
+package com.futsalground.portfolio.member.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberControllerTest {
+
+    @Test
+    public void test() {
+        System.out.println("hi test");
+    }
+}
